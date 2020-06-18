a4678aabac8bf40082fafeee3454e32301a0b757
Sebastian Roberts <tiansebas09@gmail.com>
2020-06-18 13:07:23 -0500

    Finding TLD

--- WhoIs/Form1.Designer.cs
+++ WhoIs/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.addressBox = new System.Windows.Forms.TextBox();
             this.resultBox = new System.Windows.Forms.TextBox();
             this.seartchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // addressBox
@@ -40,9 +41,13 @@
             this.addressBox.Name = "addressBox";
             this.addressBox.Size = new System.Drawing.Size(272, 21);
             this.addressBox.TabIndex = 0;
+            this.addressBox.Enter += new System.EventHandler(this.addressBox_Enter);
+            this.addressBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.addressBox_KeyDown);
+            this.addressBox.Leave += new System.EventHandler(this.addressBox_Leave);
             // 
             // resultBox
             // 
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.resultBox.Location = new System.Drawing.Point(12, 93);
             this.resultBox.Multiline = true;
             this.resultBox.Name = "resultBox";
@@ -59,18 +64,31 @@
             this.seartchButton.TabIndex = 2;
             this.seartchButton.Text = "Search";
             this.seartchButton.UseVisualStyleBackColor = true;
-            this.seartchButton.Click += new System.EventHandler(this.seartchButton_Click);
+            this.seartchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Domain Name";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(527, 411);
+            this.ClientSize = new System.Drawing.Size(510, 411);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.seartchButton);
             this.Controls.Add(this.resultBox);
             this.Controls.Add(this.addressBox);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "WhoIs";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +99,7 @@
         private System.Windows.Forms.TextBox addressBox;
         private System.Windows.Forms.TextBox resultBox;
         private System.Windows.Forms.Button seartchButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WhoIs/Form1.cs
+++ WhoIs/Form1.cs
@@ -17,9 +17,47 @@ namespace WhoIs
             InitializeComponent();
         }
 
-        private void seartchButton_Click(object sender, EventArgs e)
+        public void searchButton_Click(object sender, EventArgs e)
         {
-            Program.lookUp("whois.verisign-grs.com", addressBox.Text, resultBox);
+            resultBox.Clear();
+            char[] charArray = addressBox.Text.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            string endName = charArray[2].ToString() + charArray[1].ToString() + charArray[0].ToString();
+            resultBox.Text = endName;
+            //org whois.pir.org
+            //net whois.verisign-grs.com
+            //com whois.verisign-grs.com
+            //edu whois.educause.netS
+            //string database = "whois.verisign-grs.com";
+            //Program.lookUp(database, addressBox.Text, resultBox);
         }
+
+        private void addressBox_Enter(object sender, EventArgs e)
+        {
+            if (addressBox.Text == "Ex: google.com")
+            {
+                addressBox.Text = "";
+                addressBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void addressBox_Leave(object sender, EventArgs e)
+        {
+            if (addressBox.Text == "")
+            {
+                addressBox.Text = "Ex: google.com";
+                addressBox.ForeColor = Color.Silver;
+            }
+        }
+
+        private void addressBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                searchButton_Click(null, null);
+            }
+        }
+
+        
     }
 }
